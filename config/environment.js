818b8afcc6012c9fc76e218557604a6e8e568571
rogersillito <rogersillito@gmail.com>
2015-09-08 16:36:29 +0100

    reinstalled ember-cli to ensure versions in  sync

--- config/environment.js
+++ config/environment.js
@@ -22,12 +22,9 @@ module.exports = function(environment) {
   if (environment === 'development') {
     // ENV.APP.LOG_RESOLVER = true;
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
-    ENV.APP.LOG_TRANSITIONS = true;
+    // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
-      ENV.contentSecurityPolicy = {
-          'connect-src': "'self' https://apps.ncl.ac.uk"
-      } 
   }
 
   if (environment === 'test') {
